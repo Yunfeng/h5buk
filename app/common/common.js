892d07653af831141f4661186b47a0c4261a31d7
gezhi <gezhi@ACA80165.ipt.aol.com>
2017-02-27 23:44:03 +0800

    fixed by eslint tips

---
+++ app/common/common.js
@@ -0,0 +1,61 @@
+/**
+* 
+*/
+Date.prototype.format = function(format){
+ /*
+  * eg:format="YYYY-MM-dd hh:mm:ss";
+  */
+ var o = {
+  "M+" :  this.getMonth()+1,  //month
+  "d+" :  this.getDate(),     //day
+  "h+" :  this.getHours(),    //hour
+  "m+" :  this.getMinutes(),  //minute
+  "s+" :  this.getSeconds(), //second
+  "q+" :  Math.floor((this.getMonth()+3)/3),  //quarter
+  "S"  :  this.getMilliseconds() //millisecond
+  };
+ 
+   if(/(y+)/.test(format)) {
+    format = format.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+   }
+ 
+   for(var k in o) {
+    if(new RegExp("("+ k +")").test(format)) {
+      format = format.replace(RegExp.$1, RegExp.$1.length==1 ? o[k] : ("00"+ o[k]).substr((""+ o[k]).length));
+    }
+   }
+ return format;
+}
+
+// function convertLongToTimeDesc (l) {
+//   return getFormatDate(new Date(l))
+// }
+
+export function convertLongToTimeDesc (l) {
+  return getFormatDate(new Date(l))
+}
+
+export function getFormatDate (date, pattern) {
+  if (date === undefined) {
+    date = new Date()
+  }
+  if (pattern === undefined) {
+    pattern = 'yyyy-MM-dd hh:mm:ss'
+  }
+  return date.format(pattern)
+}
+
+export function getURLParameter (sParam) {
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) 
+  {
+      var sParameterName = sURLVariables[i].split('=');
+      if (sParameterName[0] == sParam) 
+      {
+          return sParameterName[1];
+      }
+  }
+}
+
+
---
+++ app/pages/Pnr-mobile.vue
@@ -0,0 +1,212 @@
+<template>  
+	<div id="pnr-mobile" class="container">
+    <div class="weui-toptips weui-toptips_warn" style="display:block" v-show="errAlert">{{errMsg}}</div>
+    <div id="loadingToast" v-show="loading">
+      <div class="weui-mask_transparent"></div>
+      <div class="weui-toast">
+        <i class="weui-loading weui-icon_toast"></i>
+        <p class="weui-toast__content">{{loadingText}}</p>
+      </div>
+    </div>
+
+    <div class="row  bg-info">
+      <div class="col-1">
+          <span @click="back()"><i class="fa fa-angle-left weui-tabbar__icon" aria-hidden="true"></i></span>
+      </div>         
+      <div class="col-10  bg-info text-center">
+        手机号列表 
+      </div>         
+      <div class="col-1 bg-info">
+      </div>  
+    </div>
+    <div class="row">
+      <div class="card col-12" style="padding: 0">
+        <table class="table table-striped">
+          <thead>
+              <tr>
+                  <th>手机</th>
+                  <th></th>
+                  <th>生成时间</th>
+              </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(info, index) in dataList">
+                <td>{{info.mobile}}</td>
+                <td>{{info.count}}</td>
+                <td>{{formatTime(info.createTime)}}</td>
+                </td>
+
+                <td>
+                  <span class="" @click.stop="deletePnrMobile(info)"><i class="fa fa-times" aria-hidden="true"></i></span>
+                </td>
+            </tr>
+
+          </tbody>
+        </table>
+      
+        <div class="card-block">
+          <my-pagination :row-count="sc.rowCount" :page-total="sc.pageTotal" :page-no="sc.pageNo" @next-page="nextPage" @prev-page="prevPage" @direct-page="directPage"></my-pagination>
+        </div>
+      </div>
+
+     
+
+
+
+      <div class="card col-12 card-outline-secondory">
+        <div class="card-block">
+          <input type="text" class="form-control" v-model="mobile" placeholder="手机号">
+        </div>
+        <div class="card-footer text-center">
+          <button class="btn btn-success" @click.stop="savePnrMobile()">保存</button>
+        </div>
+      </div>      
+    </div>
+  </div>
+</template>
+
+<script>
+import MyPagination from '../components/my-pagination.vue'
+import MyButton from '../components/my-button.vue'
+import MyInput from '../components/my-input.vue'
+
+import $ from 'jquery'
+import { convertLongToTimeDesc } from '../common/common.js'
+
+export default {
+  components: {
+    'my-pagination': MyPagination,
+    'my-button': MyButton,
+    'my-input': MyInput
+  },
+  data () {
+    return {
+      errAlert: false,
+      errMsg: '',
+      loading: false,
+      loadingText: '数据加载中',
+
+      dataList: [],
+      sc: {
+        rowCount: 0,
+        pageNo: 1,
+        pageSize: 25,
+        pageTotal: 0
+      },
+      mobile: ''
+    }
+  },
+  computed: {
+    // acityName() {return this.$store.state.searchParams.acityName},
+  },
+  mounted: function () {
+    this.search()
+  },
+  methods: {
+    back: function () {
+      this.$router.go(-1)
+    },
+    showErrMsg: function (msg) {
+      this.errMsg = msg
+      this.errAlert = true
+      setTimeout(() => { this.errAlert = false }, 1500)
+    },
+    search: function () {
+      var self = this
+      self.loading = true
+      self.loadingText = '数据加载中'
+
+      $.ajax({
+        type: 'get',
+        url: '/Flight/pnr/mobile',
+        data: {
+          'sc.pageNo': this.sc.pageNo,
+          'sc.pageSize': this.sc.pageSize
+        },
+        dataType: 'json',
+        success: function (jsonResult) {
+          // console.log(jsonResult)
+          self.dataList = jsonResult.dataList
+          self.sc = jsonResult.page
+        },
+        error: function (XMLHttpRequest, textStatus, errorThrown) {
+          self.searching = false
+
+          if (XMLHttpRequest.status === 403) {
+            self.$store.commit('jumpToLogin', self.$router)
+          }
+        },
+        complete: function (XMLHttpRequest, textStatus) {
+          self.loading = false
+        }
+      })
+    },
+    prevPage: function () {
+      this.sc.pageNo = this.sc.pageNo - 1
+      if (this.sc.pageNo < 1) this.sc.pageNo = 1
+      this.search()
+    },
+    nextPage: function () {
+      this.sc.pageNo = this.sc.pageNo + 1
+      this.search()
+    },
+    directPage: function (pageNo) {
+      this.sc.pageNo = pageNo
+      this.search()
+    },
+    reset: function () {
+      this.id = 0
+      this.mobile = ''
+    },
+    savePnrMobile: function () {
+      var self = this
+      $.ajax({
+        type: 'post',
+        url: '/Flight/pnr/mobile',
+        data: {
+          'mobile': self.mobile
+        },
+        dataType: 'json',
+        success: function (jsonResult) {
+          if (jsonResult !== null) {
+            if (jsonResult.status === 'OK') {
+              self.reset()
+              self.search()
+            } else {
+              self.showErrMsg('错误：' + jsonResult.errmsg)
+            }
+          }
+        }
+      })
+    },
+    deletePnrMobile: function (info) {
+      var self = this
+      $.ajax({
+        type: 'post',
+        url: '/Flight/pnr/mobile/delete',
+        data: {
+          'id': info.id
+        },
+        dataType: 'json',
+        success: function (jsonResult) {
+          if (jsonResult !== null) {
+            if (jsonResult.status === 'OK') {
+              self.search()
+            } else {
+              self.showErrMsg(jsonResult.errmsg)
+            }
+          }
+        }
+      })
+    },
+    formatTime: function (l) {
+      return convertLongToTimeDesc(l)
+    }
+  },
+  beforeRouteEnter (to, from, next) {
+    next(vm => {
+      // 通过 `vm` 访问组件实例
+    })
+  }
+}
+</script>
