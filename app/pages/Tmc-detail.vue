accdb894e66702408914aae6b1203bc5ca2e9d3b
yfdai <1410439635@qq.com>
2018-05-01 00:49:46 +0800

    show policy detail

--- app/pages/Tmc-detail.vue
+++ app/pages/Tmc-detail.vue
@@ -13,22 +13,121 @@
       <div class="card col-12">
         <table class="table table-sm">
           <tbody>
-            <tr><td class="text-right text-muted">航司</td><td>{{detail.carrier}}</td></tr>
-            <tr><td class="text-right text-muted">舱位</td><td>{{detail.subclass}}</td></tr>
-            <tr><td class="text-right text-muted">出发机场</td><td>{{getPortDesc(detail.dport)}}</td></tr>
-            <tr><td class="text-right text-muted">到达机场</td><td class="text-truncate">{{getPortDesc(detail.aport)}}</td></tr>
-            <tr><td class="text-right text-muted">返点</td><td>{{detail.returnPoint}}</td></tr>
-            <tr><td class="text-right text-muted">可开数量</td><td>{{detail.ticketLimit}}</td></tr>
-            <tr><td class="text-right text-muted">最低票面</td><td>{{detail.minPrice}}</td></tr>
-            <tr><td class="text-right text-muted">最早旅行日期</td><td>{{detail.minDdate}}</td></tr>
-            <tr><td class="text-right text-muted">最晚旅行日期</td><td>{{detail.maxDdate}}</td></tr>
-            <tr><td class="text-right text-muted">退票规则</td><td>{{detail.refundRule}}</td></tr>
-            <tr><td class="text-right text-muted">更改规则</td><td>{{detail.changeRule}}</td></tr>
-            <tr><td class="text-right text-muted">签转规则</td><td>{{detail.endorseRule}}</td></tr>
-            <tr><td class="text-right text-muted">政策类型</td><td>{{getIntlPolicyDesc(detail.intlPolicy)}}</td></tr>
-            <tr><td class="text-right text-muted">行程类型</td><td>{{getRouteTypeDesc(detail.routeType)}}</td></tr>
-            <tr><td class="text-right text-muted">备注</td><td>{{detail.remark}}</td></tr>
-            <tr><td class="text-right text-muted"></td><td>{{getStatusDesc(detail.status)}}</td></tr>
+            <tr>
+              <td class="text-right text-info" style="min-width: 5rem">航司</td>
+              <td>{{detail.carrier}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">去程舱位</td>
+              <td>{{detail.subclass}}</td>
+          </tr>
+          <tr class="backFlightInfo">
+              <td class="text-right text-info">回程舱位</td>
+              <td>{{detail.backSubclass}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">政策类型</td>
+              <td>{{getPolicyTypeDesc(detail.policyType)}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">国内国际</td>
+              <td>{{getIntlPolicyDesc(detail.intlPolicy)}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">单程往返</td>
+              <td>{{getRouteTypeDesc(detail.routeType)}}</td>
+          </tr>
+          <tr>
+            <td class="text-right">出发机场</td>
+            <td>
+              <a href="javascript:void(0)" data-toggle="tooltip" :title="detail.dport">{{getPortDesc(detail.dport)}}</a>
+              </td>
+          </tr>
+          <tr>
+            <td class="text-right">到达机场</td>
+            <td>
+              <p>
+                {{detail.aport}}
+                <span v-if="detail.excludeAirports">除外: {{detail.excludeAirports}}</span>
+              </p>
+            </td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">去程排除航班</td>
+              <td>{{detail.goExcludeFlightNo}}</td>
+          </tr>
+          <tr class="backFlightInfo">
+              <td class="text-right text-info">回程排除航班</td>
+              <td>{{detail.backExcludeFlightNo}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">去程旅行日期</td>
+              <td>开始: {{detail.minDdate}} <br />结束: {{detail.maxDdate}}</td>
+          </tr>
+          <tr class="backFlightInfo">
+              <td class="text-right text-info">回程旅行日期</td>
+              <td>开始: {{detail.minBackDate}} <br />结束: {{detail.maxBackDate}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">提前预定天数 </td>
+              <td>{{detail.advanceDays}} </td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">每用户每天可开数量 </td>
+              <td>{{detail.maxCountUserDay}} </td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">去程返点</td>
+              <td>{{detail.returnPoint}}</td>
+          </tr>
+          <tr class="backFlightInfo">
+              <td class="text-right text-info">回程返点</td>
+              <td>{{detail.backReturnPoint}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">返钱</td>
+              <td>{{detail.returnMoney}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">最低票面</td>
+              <td>{{detail.minPrice}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">最高票面</td>
+              <td>{{detail.maxPrice}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">退票规则</td>
+              <td>{{detail.refundRule}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">改期规则</td>
+              <td>{{detail.changeRule}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">签转规则</td>
+              <td>{{detail.endorseRule}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">可开数量</td>
+              <td>{{detail.ticketLimit}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">备注</td>
+              <td>{{detail.remark}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-warning">状态</td>
+              <td>{{getStatusDesc(detail.status)}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">发布时间</td>
+              <td>{{detail.createTime}}</td>
+          </tr>
+          <tr>
+              <td class="text-right text-info">修改时间</td>
+              <td>{{detail.lastupdate}}</td>
+          </tr>
           </tbody>
         </table>
       </div>  
@@ -55,6 +154,15 @@ export default {
   methods: {
     back: function () {
       this.$router.go(-1)
+    },
+    getPolicyTypeDesc: function (val) {
+      if (val === 1) {
+        return '特殊-票面低'
+      } else if (val === 0) {
+        return '普通-票面相符'
+      } else {
+        return ''
+      }
     },
     getIntlPolicyDesc: function (val) {
       var desc = '国内'
