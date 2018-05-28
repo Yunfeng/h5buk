57f3141690049412fa9cc0b24c679bf5266661ec
yfdai <1410439635@qq.com>
2018-05-28 22:23:02 +0800

    compoent: () => import()

--- app/router/router.js
+++ app/router/router.js
@@ -3,26 +3,6 @@ import Router from 'vue-router';
 
 import MyNav from '../pages/Nav.vue';
 
-const Login = () => import('../pages/Login.vue')
-const Subscribe = () => import('../pages/Register2.vue')
-const Profile = () => import('../pages/Profile.vue')
-
-const Orders = () => import('../pages/Orders.vue')
-const OrderDetail = () => import('../pages/Order-detail.vue')
-const FlightDetail = () => import('../pages/FlightDetail.vue')
-
-const Recharge = () => import('../pages/Recharge.vue')
-const MyPayResult = () => import('../pages/Recharge-result.vue')
-
-const Policies = () => import('../pages/Tmc-list.vue')
-const PolicyDetail = () => import('../pages/Tmc-detail.vue')
-const BuyTmcPolicy = () => import('../pages/Tmc-buy.vue')
-
-const FlightBookingForm1 = () => import('../pages/FlightBookingForm.vue')
-
-const FlightSearchForm = () => import('../pages/FlightSearchForm.vue')
-const FlightSearchResult = () => import('../pages/FlightSearchResult.vue')
-
 Vue.use(Router);
 
 export function createRouter () {
@@ -31,24 +11,24 @@ export function createRouter () {
     routes: [
       { path: '/', component: MyNav},
 
-      { path: '/login', component: Login},
-      { path: '/subscribe', component: Subscribe},
-      { path: '/profile', component: Profile},
+      { path: '/login', component: () => import('../pages/Login.vue')},
+      { path: '/subscribe', component: () => import('../pages/Register2.vue')},
+      { path: '/profile', component: () => import('../pages/Profile.vue')},
 
-      { path: '/recharge', component: Recharge},
-      { path: '/payresult/:id', component: MyPayResult},
+      { path: '/recharge', component: () => import('../pages/Recharge.vue')},
+      { path: '/payresult/:id', component: () => import('../pages/Recharge-result.vue')},
 
-      { path: '/orders', component: Orders, meta: { keepAlive: true } },
-      { path: '/order/:id', component: OrderDetail },
+      { path: '/orders', component: () => import('../pages/Orders.vue'), meta: { keepAlive: true } },
+      { path: '/order/:id', component: () => import('../pages/Order-detail.vue') },
 
-      { path: '/search', component: FlightSearchForm},
-      { path: '/searchResult', component: FlightSearchResult},
-      { path: '/flight/detail', component: FlightDetail},
-      { path: '/booking', component: FlightBookingForm1},
+      { path: '/search', component: () => import('../pages/FlightSearchForm.vue')},
+      { path: '/searchResult', component: () => import('../pages/FlightSearchResult.vue')},
+      { path: '/flight/detail', component: () => import('../pages/FlightDetail.vue')},
+      { path: '/booking', component: () => import('../pages/FlightBookingForm.vue')},
 
-      { path: '/policies', component: Policies},
-      { path: '/policy/:id', component: PolicyDetail},
-      { path: '/policy/:id/buy', component: BuyTmcPolicy}
+      { path: '/policies', component: () => import('../pages/Tmc-list.vue')},
+      { path: '/policy/:id', component: () => import('../pages/Tmc-detail.vue')},
+      { path: '/policy/:id/buy', component: () => import('../pages/Tmc-buy.vue')}
     ]
   });
 }
